be1e1644e84eee34b46089f02ffe051507d83b29
Javad <abdijavad110@gmail.com>
2022-05-17 09:00:25 -0400

    added serial version of bfs

---
+++ src/bfs/conf.rs
@@ -0,0 +1,5 @@
+pub const S_N: i32 = 8;
+pub const M_N: i32 = 16;
+pub const L_N: i32 = 20;
+
+pub const SPIN_DELAY: usize = 1000;
---
+++ src/bfs/mod.rs
@@ -0,0 +1,9 @@
+extern crate test;
+use test::Bencher;
+
+mod conf;
+pub mod serial;
+pub mod rayon;
+
+
+use conf::{S_N, M_N, L_N};
